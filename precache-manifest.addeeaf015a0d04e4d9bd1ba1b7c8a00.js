2336987480a0d4bb16eb52807219ba560a311c12
Mark Good <mark.gooood@gmail.com>
2019-07-06 18:44:28 -0400

    Updates

---
+++ precache-manifest.addeeaf015a0d04e4d9bd1ba1b7c8a00.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9614c966bfdea86bcd44751b083b08",
+    "url": "/loyalty-card-keeper/index.html"
+  },
+  {
+    "revision": "0b0aef7d367574b152de",
+    "url": "/loyalty-card-keeper/static/css/main.d03f8bf0.chunk.css"
+  },
+  {
+    "revision": "8ac306522c96742c9819",
+    "url": "/loyalty-card-keeper/static/js/2.ec93214b.chunk.js"
+  },
+  {
+    "revision": "0b0aef7d367574b152de",
+    "url": "/loyalty-card-keeper/static/js/main.1d6e43b0.chunk.js"
+  },
+  {
+    "revision": "af1c748eb0617a48902c",
+    "url": "/loyalty-card-keeper/static/js/runtime~main.65a5b318.js"
+  },
+  {
+    "revision": "6794d06bf09a3577b801c6cac693ee87",
+    "url": "/loyalty-card-keeper/static/media/cvs.6794d06b.png"
+  },
+  {
+    "revision": "601ebead91175613ca3477f85fbb7d77",
+    "url": "/loyalty-card-keeper/static/media/giant.601ebead.png"
+  },
+  {
+    "revision": "04e4df0b05689696bfbf2a02ad976496",
+    "url": "/loyalty-card-keeper/static/media/safeway.04e4df0b.png"
+  },
+  {
+    "revision": "cb9737f3aabc3b1785bb6bcfc0d70c30",
+    "url": "/loyalty-card-keeper/static/media/vic.cb9737f3.png"
+  }
+]);
