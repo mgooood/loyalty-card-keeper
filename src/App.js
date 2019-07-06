a9d4b1c20eae5d265f573fd5971eb2019f56809e
Mark Good <mark.gooood@gmail.com>
2019-07-06 18:44:59 -0400

    adding starting files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import vic from './images/vic.png';
+import safeway from './images/safeway.png';
+import giant from './images/giant.png';
+import cvs from './images/cvs.png';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loyaltyCards: [
+        {
+          id: 1,
+          imgSrc: vic,
+          name: 'VIC'
+        },
+        {
+          id: 2,
+          imgSrc: safeway,
+          name: 'Safeway'
+        },
+        {
+          id: 3,
+          imgSrc: giant,
+          name: 'Giant'
+        },
+        {
+          id: 4,
+          imgSrc: cvs,
+          name: 'CVS'
+        }
+      ],
+      searchField: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }  
+
+  render() {
+    const { loyaltyCards, searchField } = this.state;
+    const filteredLoyaltyCards = loyaltyCards.filter( card => card.name.toLowerCase().includes(searchField.toLowerCase()));
+    return (
+      <div className='App'>
+        <h1 className='title'>Loyalty Card Store</h1>
+        <SearchBox 
+          placeholder='search loyalty cards'
+          handleChange={this.handleChange}
+        />
+        <CardList loyaltyCards={filteredLoyaltyCards} />
+      </div>
+    );
+  }
 }
 
 export default App;
